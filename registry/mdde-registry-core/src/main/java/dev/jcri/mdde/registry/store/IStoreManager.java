8fa7170d7f19d6d00c7f33bb61e391f0ec7d2aaa
Andrey Kharitonov <32765888+akharitonov@users.noreply.github.com>
2020-01-28 20:12:36 +0100

    MDDE-Registry: Save and restore registry snapshots for Redis implementation

---
+++ registry/mdde-registry-core/src/main/java/dev/jcri/mdde/registry/store/IStoreManager.java
@@ -0,0 +1,25 @@
+package dev.jcri.mdde.registry.store;
+
+import java.io.IOException;
+
+/**
+ * Interface that must be implemented by the registry store implementation in order for the registry to be able to
+ * perform management operations such as making snapshots, restoration from a file
+ */
+public interface IStoreManager {
+    /**
+     * Save all of the current data nodes data snapshots to a file.
+     * Database specific file, meant for later restore by restoreFromFile(...)
+     * @param pathToFile Full file name
+     * @param overwrite Overwrite if the file already exits
+     * @return True - data was saved successfully
+     */
+    boolean dumpToFile(String pathToFile, boolean overwrite) throws IOException;
+
+    /**
+     * Restore data from a file
+     * @param pathToFile Full file name
+     * @return True - data was restored successfully
+     */
+    boolean restoreFromFile(String pathToFile) throws IOException;
+}
---
+++ registry/mdde-registry-core/src/main/java/dev/jcri/mdde/registry/store/impl/redis/RedisStoreManager.java
@@ -0,0 +1,137 @@
+package dev.jcri.mdde.registry.store.impl.redis;
+
+import dev.jcri.mdde.registry.store.IStoreManager;
+import dev.jcri.mdde.registry.utility.ByteTools;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.Response;
+import redis.clients.jedis.ScanResult;
+
+import java.io.*;
+import java.nio.channels.FileLock;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class RedisStoreManager implements IStoreManager {
+    private static final Logger logger = LogManager.getLogger(RedisStoreManager.class);
+
+    private RedisConnectionHelper _redisConnection;
+
+    public RedisStoreManager(RedisConnectionHelper redisConnectionHelper){
+        Objects.requireNonNull(redisConnectionHelper, "Redis connection helper class can't be set tu null");
+        _redisConnection = redisConnectionHelper;
+    }
+
+
+    private static final Charset _dumpCharset = StandardCharsets.UTF_8;
+    @Override
+    public boolean dumpToFile(String pathToFile, boolean overwrite) throws IOException {
+        File dumpFile = new File(pathToFile);
+        if(dumpFile.exists()) {
+            if(dumpFile.isDirectory()){
+                throw new IOException(String.format("Specified path is a directory: '%s'", pathToFile));
+            }
+            if(!overwrite){
+                throw new FileAlreadyExistsException(String.format("Specified file already exists: '%s'", pathToFile));
+            }
+            else{
+                if(!dumpFile.delete()){
+                    throw new IOException(String.format("Unable to overwrite the existing file: '%s'", pathToFile));
+                }
+            }
+        }
+
+        try(FileOutputStream out = new FileOutputStream(dumpFile)) {
+            try (FileLock outLock = out.getChannel().lock()) {
+                try(Jedis jedis = _redisConnection.getRedisCommands()){
+                    // |number of keys in the registry instance of Redis|
+                    // 8 bytes
+                    var nKeys = jedis.dbSize();
+                    out.write(ByteTools.longToByteArray(nKeys));
+
+                    String scanCursor = "0";
+                    while(true){
+                        // Read one by one, slower but easier on RAM
+                        ScanResult<String> scan = jedis.scan(scanCursor);
+                        scanCursor = scan.getCursor();
+                        if(scan.getResult() != null && scan.getResult().size() > 0){
+                            // Dump keys
+                            for(String key: scan.getResult()){
+                                byte[] value = jedis.dump(key);
+                                // |key len|key val|value len|value val|
+                                // |4 bytes|...    |4 bytes  |...      |
+                                // Key
+                                var keyBytes = key.getBytes(_dumpCharset);
+                                out.write(ByteTools.intToByteArray(keyBytes.length));
+                                out.write(keyBytes);
+                                // Value
+                                out.write(ByteTools.intToByteArray(value.length));
+                                out.write(value);
+                            }
+                        }
+
+                        if(scan.isCompleteIteration()){
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean restoreFromFile(String pathToFile) throws IOException {
+        File dumpFile = new File(pathToFile);
+        if(!dumpFile.exists()) {
+            throw new FileNotFoundException("Unable to find the Redis dump file");
+        }
+
+        try(FileInputStream in = new FileInputStream(dumpFile)) {
+            // Read number of keys in the node
+            final long numKeys = ByteTools.byteArrayToLong(in.readNBytes(8));
+            long keysProcessed = 0;
+            final int batchSize = 25; // # number of keys to read and restore at a time
+            while(keysProcessed < numKeys){
+                final long batchLimit = Math.min(numKeys, keysProcessed + batchSize);
+                Map<String, byte[]> keyValues = new HashMap<>();
+                for(; keysProcessed < batchLimit; keysProcessed++){
+                    // Read key
+                    int keyLength = ByteTools.byteArrayToInt(in.readNBytes(4));
+                    String key = new String(in.readNBytes(keyLength), _dumpCharset);
+                    // Read value
+                    int valueLength = ByteTools.byteArrayToInt(in.readNBytes(4));
+                    byte[] value = in.readNBytes(valueLength);
+                    keyValues.put(key, value);
+                }
+                Map<String, Response<String>> responseMap = new HashMap<>();
+                try(Jedis jedis = _redisConnection.getRedisCommands()) {
+                    try(Pipeline p = jedis.pipelined()) {
+                        for (var keyValue : keyValues.entrySet()) {
+                            Response<String> rResponse = p.restore(keyValue.getKey(), 0, keyValue.getValue());
+                            responseMap.put(keyValue.getKey(), rResponse);
+                        }
+                    }
+                }
+                // Make sure all were restored
+                var failedKeys = responseMap.entrySet()
+                        .stream()
+                        .filter(e -> !e.getValue().get().equals("OK"))
+                        .map(Map.Entry::getKey).collect(Collectors.toSet());
+                if(failedKeys.size() > 0) {
+                    throw new IOException(
+                            String.format("Failed to restore for following keys: %s",
+                                            String.join(",", failedKeys)));
+                }
+            }
+        }
+        return true;
+    }
+}
