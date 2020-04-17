a54a711d7dbd55b39bd8486a114c1e603c11eb2c
akharitonov <32765888+akharitonov@users.noreply.github.com>
2020-04-17 21:23:12 +0200

    Removed host port mapping from redis docker-compose

--- docker/compositions/redis/start_redis_only.sh
+++ docker/compositions/redis/start_redis_only.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Bring up only Redis nodes. Useful for debugging code locally
-docker-compose -f docker-compose.yml up registry_store redis_node_0 redis_node_1 redis_node_2 redis_node_3
+docker-compose -f docker-compose.yml -f docker-compose.debug.yml up registry_store redis_node_0 redis_node_1 redis_node_2 redis_node_3
