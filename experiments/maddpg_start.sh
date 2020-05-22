db06cf18617fca781587843c6bf0a35d487c436a
akharitonov <32765888+akharitonov@users.noreply.github.com>
2020-05-22 14:20:31 +0200

    Sample MADDPG experiment: added gamma 0.5 experiments
    git-cherry-pick from master@4ca1ae189fa03b131c32c58cc64b05929d8e87ff

--- experiments/maddpg_start.sh
+++ experiments/maddpg_start.sh
@@ -22,6 +22,16 @@ rm ${COMPOSE_DIR}/${ARGS_FILE}
 echo "LAUNCH_ARGS=--no-do-nothing" > ${COMPOSE_DIR}/${ARGS_FILE}
 (cd ${COMPOSE_DIR}/scripts && sh maddpg_start_detached.sh ${PFX}maddpg_wdn${POSTFX})
 
+# With do-nothing
+rm ${COMPOSE_DIR}/${ARGS_FILE}
+echo "LAUNCH_ARGS=--gamma 0.5" > ${COMPOSE_DIR}/${ARGS_FILE}
+(cd ${COMPOSE_DIR}/scripts && sh maddpg_start_detached.sh ${PFX}maddpg_dn_g05)
+
+# Without do-nothing
+rm ${COMPOSE_DIR}/${ARGS_FILE}
+echo "LAUNCH_ARGS=--no-do-nothing --gamma 0.5" > ${COMPOSE_DIR}/${ARGS_FILE}
+(cd ${COMPOSE_DIR}/scripts && sh maddpg_start_detached.sh ${PFX}maddpg_wdn_g05)
+
 # Cleanup the args file
 rm ${COMPOSE_DIR}/${ARGS_FILE}
 echo "" > ${COMPOSE_DIR}/${ARGS_FILE}
