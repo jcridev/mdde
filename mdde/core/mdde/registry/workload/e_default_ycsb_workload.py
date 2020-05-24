c8cf8d4707009c899aef9492de97ad35c6f0d71d
akharitonov <32765888+akharitonov@users.noreply.github.com>
2020-05-24 14:40:08 +0200

    New predefined workloads with low record count, high read op count
    READ_100000_ZIPFIAN;
    READ_100000_LATEST;
    READ_100000_UNIFORM;
    READ_1000_10000_LATEST;
    READ_1000_100000_LATEST;
    READ_10000_100000_LATEST;
    READ_10000_1000000_LATEST;

--- mdde/core/mdde/registry/workload/e_default_ycsb_workload.py
+++ mdde/core/mdde/registry/workload/e_default_ycsb_workload.py
@@ -26,6 +26,24 @@ class EDefaultYCSBWorkload(Enum):
                                           record_count=10000,
                                           operation_count=10000)
 
+    READ_100000_ZIPFIAN = YCSBWorkloadInfo(tag='read100000zipfian',
+                                           request_distribution=EYCSBWorkloadRequestDistribution.ZIPFIAN,
+                                           read_all_fields=True,
+                                           record_count=100000,
+                                           operation_count=100000)
+
+    READ_100000_LATEST = YCSBWorkloadInfo(tag='read100000latest',
+                                          request_distribution=EYCSBWorkloadRequestDistribution.LATEST,
+                                          read_all_fields=True,
+                                          record_count=100000,
+                                          operation_count=100000)
+
+    READ_100000_UNIFORM = YCSBWorkloadInfo(tag='read100000uniform',
+                                           request_distribution=EYCSBWorkloadRequestDistribution.UNIFORM,
+                                           read_all_fields=True,
+                                           record_count=100000,
+                                           operation_count=100000)
+
     READ_10000_1000_LATEST = YCSBWorkloadInfo(tag='read10000o1000latest',
                                               request_distribution=EYCSBWorkloadRequestDistribution.LATEST,
                                               read_all_fields=True,
@@ -49,3 +67,27 @@ class EDefaultYCSBWorkload(Enum):
                                                 read_all_fields=True,
                                                 record_count=2000000,
                                                 operation_count=1000)
+
+    READ_1000_10000_LATEST = YCSBWorkloadInfo(tag='read1000o10000latest',
+                                              request_distribution=EYCSBWorkloadRequestDistribution.LATEST,
+                                              read_all_fields=True,
+                                              record_count=1000,
+                                              operation_count=10000)
+
+    READ_1000_100000_LATEST = YCSBWorkloadInfo(tag='read1000o100000latest',
+                                               request_distribution=EYCSBWorkloadRequestDistribution.LATEST,
+                                               read_all_fields=True,
+                                               record_count=1000,
+                                               operation_count=100000)
+
+    READ_10000_100000_LATEST = YCSBWorkloadInfo(tag='read10000o100000latest',
+                                                request_distribution=EYCSBWorkloadRequestDistribution.LATEST,
+                                                read_all_fields=True,
+                                                record_count=10000,
+                                                operation_count=100000)
+
+    READ_10000_1000000_LATEST = YCSBWorkloadInfo(tag='read10000o1000000latest',
+                                                 request_distribution=EYCSBWorkloadRequestDistribution.LATEST,
+                                                 read_all_fields=True,
+                                                 record_count=10000,
+                                                 operation_count=1000000)
