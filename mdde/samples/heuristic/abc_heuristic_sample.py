7cd36ed97b0b543c503f1a436e67ccf3926a2e7a
akharitonov <32765888+akharitonov@users.noreply.github.com>
2020-06-06 21:57:11 +0200

    Adjusted heuristic sample metrics

--- mdde/samples/heuristic/abc_heuristic_sample.py
+++ mdde/samples/heuristic/abc_heuristic_sample.py
@@ -68,7 +68,7 @@ class ABCMDDEHeuristicSample(ABC):
         throughput_all: Dict = {}
         real_reads, real_throughput = self.processBenchmarkStatsInEnv(env._bench_request_stats(), env)
         throughput_all[-1] = real_throughput
-        magnitude_variations = [0.1, 0.5, 1, 1.5, 1.75, 2.0, 2.3, 2.5]
+        magnitude_variations = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 
         for magnitude in magnitude_variations:
             bench_response = env._bench_request_stats_counterfeit(magnitude_override=magnitude)
