09082b3c1ad0fc8f96b0a3bf67d28e1df90df6b8
akharitonov <32765888+akharitonov@users.noreply.github.com>
2020-06-12 19:53:31 +0200

    Action mask experiment set is now estimation oritnted

--- experiments/maddpg_action-mask_start.sh
+++ experiments/maddpg_action-mask_start.sh
@@ -3,6 +3,11 @@
 # Prfix for container names
 PFX=${1:-""}
 
+# Simulation (benchmark estimation flag)
+SIM=" --sim"
+
+SLEEP_BETWEEN=1
+
 # *.env file used by docker-compose
 ARGS_FILE=args.env
 COMPOSE_DIR=../docker/compositions/redis
@@ -12,33 +17,42 @@ COMPOSE_DIR=../docker/compositions/redis
 
 # With do-nothing
 rm ${COMPOSE_DIR}/${ARGS_FILE}
-echo "" > ${COMPOSE_DIR}/${ARGS_FILE}
+echo "${SIM}" > ${COMPOSE_DIR}/${ARGS_FILE}
 (cd ${COMPOSE_DIR}/scripts && sh maddpg_act-mask_start_detached.sh ${PFX}maddpg_am_dn)
+
+if [ $SLEEP_BETWEEN == 1 ]; then sleep 5m; fi 
 
 # Without do-nothing
 rm ${COMPOSE_DIR}/${ARGS_FILE}
-echo "LAUNCH_ARGS=--no-do-nothing" > ${COMPOSE_DIR}/${ARGS_FILE}
+echo "LAUNCH_ARGS=--no-do-nothing${SIM}" > ${COMPOSE_DIR}/${ARGS_FILE}
 (cd ${COMPOSE_DIR}/scripts && sh maddpg_act-mask_start_detached.sh ${PFX}maddpg_am_wdn)
 
+if [ $SLEEP_BETWEEN == 1 ]; then sleep 5m; fi 
+
 # With do-nothing, disregard storage
 rm ${COMPOSE_DIR}/${ARGS_FILE}
-echo "LAUNCH_ARGS=--store-m 0.0" > ${COMPOSE_DIR}/${ARGS_FILE}
+echo "LAUNCH_ARGS=--store-m 0.0${SIM}" > ${COMPOSE_DIR}/${ARGS_FILE}
 (cd ${COMPOSE_DIR}/scripts && sh maddpg_act-mask_start_detached.sh ${PFX}maddpg_am_dn_sm0)
 
+if [ $SLEEP_BETWEEN == 1 ]; then sleep 5m; fi 
+
 # With do-nothing, disregard storage
 rm ${COMPOSE_DIR}/${ARGS_FILE}
-echo "LAUNCH_ARGS=--no-do-nothing --store-m 0.0" > ${COMPOSE_DIR}/${ARGS_FILE}
+echo "LAUNCH_ARGS=--no-do-nothing --store-m 0.0${SIM}" > ${COMPOSE_DIR}/${ARGS_FILE}
 (cd ${COMPOSE_DIR}/scripts && sh maddpg_act-mask_start_detached.sh ${PFX}maddpg_am_wdn_sm0)
 
+# if [ $SLEEP_BETWEEN == 1 ]; then sleep 5m; fi 
 
 # With do-nothing, gamma=0.5
 #rm ${COMPOSE_DIR}/${ARGS_FILE}
-#echo "LAUNCH_ARGS=--gamma 0.5" > ${COMPOSE_DIR}/${ARGS_FILE}
+#echo "LAUNCH_ARGS=--gamma 0.5 --sim" > ${COMPOSE_DIR}/${ARGS_FILE}
 #(cd ${COMPOSE_DIR}/scripts && sh maddpg_act-mask_start_detached.sh ${PFX}maddpg_dn_g05)
+
+# if [ $SLEEP_BETWEEN == 1 ]; then sleep 5m; fi 
 
 # Without do-nothing, gamma=0.5
 #rm ${COMPOSE_DIR}/${ARGS_FILE}
-#echo "LAUNCH_ARGS=--no-do-nothing --gamma 0.5" > ${COMPOSE_DIR}/${ARGS_FILE}
+#echo "LAUNCH_ARGS=--no-do-nothing --gamma 0.5 --sim" > ${COMPOSE_DIR}/${ARGS_FILE}
 #(cd ${COMPOSE_DIR}/scripts && sh maddpg_act-mask_start_detached.sh ${PFX}maddpg_wdn_g05)
 
 # Cleanup the args file
